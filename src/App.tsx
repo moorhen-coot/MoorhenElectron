cf1abd3c2aee0f51e5753d09502093d93261deae
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-04-03 10:09:52 +0100

    Update to Moorhen 0.8.3 (37f9e29a20a5846922e4ec4f4dd9c4c431089478).

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useRef } from 'react';
+
 import './App.css';
 import './moorhen.css';
 import { MoorhenContainer } from 'moorhen';
 import { MoorhenReduxProvider } from "moorhen";
 
 function App() {
+  const glRef = useRef(null)
+  const commandCentre = useRef(null)
+  const collectedProps = {
+        glRef,  commandCentre
+    }
   return (
     <div className="App">
       <MoorhenReduxProvider>
-        <MoorhenContainer/>
+        <MoorhenContainer  {...collectedProps}/>
       </MoorhenReduxProvider>
     </div>
   );
