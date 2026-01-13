18334da951cd2521075d13ddcecd27789673f62d
Clement Degut <c.degut@gmail.com>
2026-01-13 15:56:52 +0000

    update to moorhen1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useRef } from 'react';
-import { MoorhenContainer, MoorhenReduxStore } from 'moorhen';
-import { Provider } from 'react-redux';
-
-import './App.css';
-import './moorhen.css';
+import { useRef } from "react";
+import { MoorhenContainer, MoorhenProvider } from "moorhen";
 
 export const MyMoorhenContainer = (props) => {
-
-    const glRef = useRef(null)
-    const commandCentre = useRef(null)
-    const moleculesRef = useRef(null)
-    const mapsRef = useRef(null)
+    const glRef = useRef(null);
+    const commandCentre = useRef(null);
+    const moleculesRef = useRef(null);
+    const mapsRef = useRef(null);
 
     const collectedProps = {
-        glRef,  commandCentre, moleculesRef, mapsRef
-    }
+        glRef,
+        commandCentre,
+        moleculesRef,
+        mapsRef,
+    };
 
-    return (
-        <MoorhenContainer {...collectedProps}/>
-    )
-
-}
+    return <MoorhenContainer {...collectedProps} />;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <Provider store={MoorhenReduxStore}>
-        <MyMoorhenContainer/>
-      </Provider>
-    </div>
-  );
+    return (
+        <div>
+            <MoorhenProvider>
+                <MoorhenContainer />
+            </MoorhenProvider>
+        </div>
+    );
 }
 
 export default App;
